ca50ca7fbc0189ec7f18a5a7dc0b78d49411a0f3
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-05-17 17:25:54 +0500

    Updated

--- 03-Chapter/HelloWorld.cpp
+++ 03-Chapter/HelloWorld.cpp
@@ -1,10 +1,9 @@
-#include<iostream> //Header Library used for Input and Output
+#include<cstdio> //Header Library used for Input and Output
 using namespace std; //This define the names of IO libraries
 
 int main() //This is the entry point or function under which the program will run
 {
-       cout <<"Number 1";
-       cout <<"Number 2";
+       puts("Hello world");
        return 0;
 
 }
