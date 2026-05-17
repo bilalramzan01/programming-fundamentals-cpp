5594ab05506a4ee122c94aa6e4983879714ab21c
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-05-17 14:50:21 +0500

    Updated

---
+++ 03-Output/03-NewLine.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//   To insert a new line in your output, you can use the \n character:
+
+  cout << "Hello World! \n";
+  cout << "I am learning C++"<<"\n";
+  cout << "Hello World!" << "\n";
+  cout << "I am learning C++"<<endl;
+   cout << "Hello World!" << "\t";
+  cout << "I am learning C++";
+  return 0;
+}
