8a872ae0cfc750f20eeeccf0bef6dedb90196137
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-05-17 15:15:00 +0500

    updated

---
+++ 04-Comments/comments.cpp
@@ -0,0 +1,4 @@
+// Single-line Comments
+// Single-line comments start with two forward slashes (//).
+
+// Any text between // and the end of the line is ignored by the compiler (will not be executed).
