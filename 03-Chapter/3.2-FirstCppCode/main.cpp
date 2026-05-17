311f673be89eb1afe783784ba469ec18ac839470
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-05-17 16:58:50 +0500

    UPDATED

---
+++ 03-Chapter/3.2-FirstCppCode/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream> //Header Library used for Input and Output
+using namespace std; //This define the names of IO libraries
+
+int main() //This is the entry point or function under which the program will run
+{
+       cout <<"Number 1";
+       cout <<"Number 2";
+       return 0;
+
+}
+0
--- 04-Comments/comments.cpp
+++
@@ -1,4 +0,0 @@
-// Single-line Comments
-// Single-line comments start with two forward slashes (//).
-
-// Any text between // and the end of the line is ignored by the compiler (will not be executed).
