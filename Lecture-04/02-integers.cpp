2c63b04a2427750763e140db897b6b9e7b7901ae
bilalramzan01 <bilalramzan.cs@gmail.com>
2026-05-17 13:24:25 +0500

    updated

--- Lecture-04/02-integers.cpp
+++ Lecture-04/02-integers.cpp
@@ -3,9 +3,11 @@ using namespace std;
 int main()
 {
 
-    int A;  //Variable decalaration 
-    //Interger take 4 bytes of memory
+    int A;  //Variable decalaration   //Interger take 4 bytes of memory
     A = 32; //Value Assign to variable
-    cout << A;
+    cout << hex<< A<<endl;
+    cout<<&A<<endl;
+    cout<<sizeof(A);
+    return 0;
 
-}
+}   
